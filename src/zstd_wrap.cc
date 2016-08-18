52fe0fa20d8b35f10f21a77916600e212ad71568
Zwb <zwb.ict@gmail.com>
2016-08-18 15:41:51 +0800

    feat<zstd_wrap>: imple Decompress

--- src/zstd_wrap.cc
+++ src/zstd_wrap.cc
@@ -38,7 +38,44 @@ namespace {
     }
 
     size_t cSize = ZSTD_compress(dst, dstCapacity, src, srcSize, compressionLevel);
+    if (ZST_isError(cSize)) {
+      return NanThrow("Compress failed!");
+    }
+
     Local<Object> dstBuf = NewBuffer(dst, cSize);
+    free(dst);
+
+    return info.GetReturnValue().Set(dstBuf);
+  }
+
+  NAN_METHOD(Decompress) {
+
+    ZSTDWrap *obj = ObjectWrap::Unwrap<ZSTDWrap>(info.Holder());
+
+    if (info.Length() < 1 || !HasInstance(info[0])) {
+      return NanThrow("First argument should be a buffer");
+    }
+
+    Local<Ojbect> buf = info[0]->ToObject();
+    char *src = Data(buf);
+    sizt_t srcSize = Length(buf);
+
+    size_t dstCapacity = ZSTD_getDecompressedSize(src, srcSize);
+    if (0 == dstCapacity) {
+      return NanThrow("Compressed size unknown");
+    }
+
+    char *dst = malloc(dstCapacity);
+    if (NULL == dst) {
+      return NanThrow("Too large, Out of memory!");
+    }
+
+    size_t dSize = ZSTD_decompress(dst, dstCapacity, src, srcSize);
+    if (ZSTD_isError(dSize))  {
+      return NanThrow("Decompress failed!");
+    }
+
+    Local<Object> dstBuf = NewBuffer(dst, dSize);
     free(dst);
 
     return info.GetReturnValue().Set(dstBuf);
