a5fd3a19185ed2785f93dbfeb8b339ce949f0d49
Zwb <zwb.ict@gmail.com>
2016-08-19 04:15:18 +0800

    test<zstd>: add sample case to test compress, decompress

---
+++ tests/zstd.js
@@ -0,0 +1,10 @@
+var zstd = require('./../'),
+    assert = require('assert');
+
+var buf = new Buffer('hello zstd');
+var dbuf = zstd.decompress(zstd.compress(buf, 18));
+assert(buf.toString() === dbuf.toString());
+
+var dict = new Buffer('Nice to meet you!');
+var ddbuf = zstd.decompressUsingDict(zstd.compressUsingDict(buf, dict, 18), dict);
+assert(buf.toString() == ddbuf.toString());
