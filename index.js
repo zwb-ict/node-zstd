11bfbcffec0f5acc3b037b1cffb97a2381beb827
Zwb <zwb.ict@gmail.com>
2016-08-18 17:21:05 +0800

    fix<index>: fix misspelling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var addon =  require('bindings')('node_zstd.node');
 
-module.exports.compress = addon.compress;
-module.exports.decompress = addon.decompress;
+module.exports.compress = addon.Compress;
+module.exports.decompress = addon.Decompress;
