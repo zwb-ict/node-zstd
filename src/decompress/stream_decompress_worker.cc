426e5a03dbba3e5928ff0b90fe1c47472e7f2c9d
Zwb <zwb.ict@gmail.com>
2016-09-29 18:52:58 +0800

    feat<stream_decompress_worker>: impl StreamDecompressWorker

---
+++ src/decompress/stream_decompress_worker.cc
@@ -0,0 +1,74 @@
+#include "stream_decompress_worker.h"
+
+namespace ZSTD_NODE {
+
+  using Nan::HandleScope;
+  using Nan::Callback;
+  using Nan::Error;
+
+  using v8::String;
+  using v8::Local;
+  using v8::Value;
+
+  StreamDecodeWorker::StreamDecodeWorker(Callback *callback, StreamDecode* sd)
+    : AsyncWorker(callback), sd(sd) {
+    zInBuf = {sd->input, sd->inputSize, 0};
+    size_t dstSize = ZSTD_DStreamOutSize();
+    void *dst = sd->alloc.Alloc(dstSize);
+    zOutBuf = {dst, dstSize, 0};
+  }
+
+  StreamDecodeWorker::~StreamDecodeWorker() {
+    sd->alloc.Free(zOutBuf.dst);
+  }
+
+  void StreamDecodeWorker::Execute() {
+    while (zInBuf.pos < zInBuf.size) {
+      do {
+        zOutBuf.pos = 0;
+        ret = ZSTD_decompressStream(sd->zds, &zOutBuf, &zInBuf);
+        if (ZSTD_isError(ret)) {
+          SetErrorMessage(ZSTD_getErrorName(ret));
+        }
+        pushToPendingOutput();
+      } while (ret == 1);
+    }
+  }
+
+  void StreamDecodeWorker::pushToPendingOutput() {
+    char *output = static_cast<char*>(sd->alloc.Alloc(zOutBuf.pos));
+    if (output == NULL) {
+      SetErrorMessage("ZSTD decompress failed, out of memory");
+    }
+    memcpy(output, zOutBuf.dst, zOutBuf.pos);
+    Allocator::AllocatedBuffer* buf_info = Allocator::GetBufferInfo(output);
+    buf_info->available = 0;
+    sd->pending_output.push_back(output);
+  }
+
+  void StreamDecodeWorker::HandleOKCallback() {
+    HandleScope scope;
+
+    const int argc = 2;
+    Local<Value> argv[argc] = {
+      Nan::Null(),
+      sd->PendingChunksAsArray()
+    };
+    callback->Call(argc, argv);
+
+    sd->alloc.ReportMemoryToV8();
+  }
+
+  void StreamDecodeWorker::HandleErrorCallback() {
+    HandleScope scope;
+
+    const int argc = 1;
+    Local<Value> argv[argc] = {
+      Error(Nan::New<String>(ErrorMessage()).ToLocalChecked())
+    };
+    callback->Call(argc, argv);
+
+    sd->alloc.ReportMemoryToV8();
+  }
+
+}
